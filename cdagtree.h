e760b41d89a710030414437a0de03f29d20545d4
ghyangyh <ghyangyh1984@gmail.com>
2020-12-14 10:14:53 +0800

    add new functionalities to cdagtree and main

--- cdagtree.h
+++ cdagtree.h
@@ -7,6 +7,7 @@
 #include <sstream>
 #include <iostream>
 #include <memory>
+#include <limits>
 
 #include <Eigen/Dense>
 
@@ -29,7 +30,9 @@ struct CDAGNode {
     std::vector<std::shared_ptr<CDAGNode>> m_child_nodes;
 };
 
-
+/*
+ * The branch consists of a number of nodes.
+*/
 template<typename T>
 class CBranch {
 public:
@@ -55,11 +58,20 @@ public:
     void add_node(const std::shared_ptr<CDAGNode<T>>& aNodePtr) {
         m_branch_nodes.push_back(aNodePtr);
     }
+    std::vector<std::shared_ptr<CDAGNode<T>>>& get_branch_nodes() {
+        return m_branch_nodes;
+    }
+    const std::vector<std::shared_ptr<CDAGNode<T>>>& get_branch_nodes() const {
+        return m_branch_nodes;
+    }
 private:
-    int m_branch_level;
-    std::vector<std::shared_ptr<CDAGNode<T>>> m_branch_nodes;
+    int m_branch_level;                                         // the branch level, e.g. the trunk has level 1.
+    std::vector<std::shared_ptr<CDAGNode<T>>> m_branch_nodes;   // store all the nodes belongs to this branch by shared pointers.
 };
 
+/*
+ * Store all the branches having the same level id.
+*/
 template<typename T>
 class CBranchLevelSet {
 public:
@@ -70,6 +82,7 @@ public:
     }
     CBranchLevelSet& operator=(const CBranchLevelSet& aRhs) {
         if(*this != &aRhs) {
+            m_branch_array.clear();
             m_branch_array = aRhs.m_branch_array;
         }
         return (*this);
@@ -78,13 +91,23 @@ public:
     void add_branch(const std::shared_ptr<CBranch<T>>& aBranchPtr) {
         m_branch_array.push_back(aBranchPtr);
     }
+    std::vector<std::shared_ptr<CBranch<T>>>& get_branch_array() {
+        return m_branch_array;
+    }
+    const std::vector<std::shared_ptr<CBranch<T>>>& get_branch_array() const {
+        return m_branch_array;
+    }
 private:
     std::vector<std::shared_ptr<CBranch<T>>> m_branch_array;
 };
 
+/*
+ * The Directed-Acyclic Tree. A adjacency-list representation is adopted here.
+*/
 template<typename T>
 class CDAGTree
 {
+    typedef Eigen::Matrix<T, 3, 1> Vector3t;
 public:
     CDAGTree();
     ~CDAGTree();
@@ -102,17 +125,57 @@ public:
      * Extract branches from the tree graph.
      * The branches are extracted by level. E.g. the trunk branch is at level 1,
      * and all the branches directly connected to the trunk branch is at level 2, so on so forth...
+     * This is only a dirver function of a recursive extraction funtion.
     */
     void extract_branches();
 
     size_t get_total_num_of_nodes() const {
         return m_node_array.size();
     }
+
+    size_t get_total_num_of_branch_levels() const {
+        return m_branches_array.size();
+    }
 protected:
+    /*
+     * Build the tree graph from a parent node in a depth-first recursive way.
+     * Params:
+     * inputs: the input file stream containing the nodes information
+     * aParentNodePtr: the pointer to the node where the tree nodes are to be handled
+     * aChildNum: the number of child nodes of the current node.
+     * The member array m_node_array is modified after calling this function.
+    */
     void build_tree_graph_recursive(std::ifstream& inputs, std::shared_ptr<CDAGNode<T>>& aParentNodePtr, int aChildrenNum);
-    void extract_branch_at_level(int aLevel, const std::shared_ptr<CDAGNode<T>>& aBranchRootNodePtr, CBranch<T>& aBranch);
+
+    /*
+     * Extract all the branches from a parent node recursively.
+     * Params:
+     * aLevel: the branch level
+     * aParentNodePtr: the parent node to start.
+    */
+    void extract_branches_recursive(int aLevel,
+                                    std::vector<std::shared_ptr<CDAGNode<T>>>& aPreviousLevelBranch);
+
+    /*
+     * A utility function to extract the branche at a specified level.
+     * For the trunk (level 1), all the nodes with their direction to the parent node
+     * closest to the 'up direction' is treated as a candidate trunk node. The 'up direction'
+     * of the trunk is set as the (0, 1, 0) direction in this implementation.
+     * For other branches, the 'up direction' is defined as the direction from the first child node
+     * to its parent node at the parent branch of the previous tree level.
+     * Params:
+     * aLevel: the branch level
+     * aBranchRootNodePtr: a pointer to the branch root node
+     * aUpDir: the up direction for the branch
+     * aBranch: the branch containing all the extracted nodes.
+    */
+    void extract_branch_at_level(int aLevel,
+                                 const std::shared_ptr<CDAGNode<T>>& aBranchRootNodePtr,
+                                 const Vector3t& aUpDir,
+                                 CBranch<T>& aBranch);
 private:
-    std::vector<std::shared_ptr<CDAGNode<T>>> m_node_array;
+    std::vector<std::shared_ptr<CDAGNode<T>>> m_node_array; // store all the tree nodes using shared pointers.
+    std::vector<CBranchLevelSet<T>> m_branches_array;       // store all the branches at different levels
 };
 
 template<typename T>
@@ -189,36 +252,89 @@ void CDAGTree<T>::build_tree_graph_recursive(std::ifstream& inputs, std::shared_
 }
 
 template<typename T>
-void CDAGTree<T>::extract_branch_at_level(int aLevel, const std::shared_ptr<CDAGNode<T>>& aBranchRootNodePtr, CBranch<T>& aBranch) {
-    typedef Eigen::Matrix<T, 3, 1> Vector3T;
-    if(aLevel == 1) { // the trunk is at level 1
-        Vector3T a_up_dir(0, 1, 0);
-        if(aBranchRootNodePtr->m_child_nodes.empty())
+void CDAGTree<T>::extract_branch_at_level(int aLevel,
+                                          const std::shared_ptr<CDAGNode<T>>& aBranchRootNodePtr,
+                                          const Vector3t& aUpDir,
+                                          CBranch<T>& aBranch) {
+        if(aBranchRootNodePtr->m_child_nodes.empty())   // reach a leaf node
             return;
-        // find the internode between the child nodes that is in a
-        // direction closest to the up dir;
+
+        // find the internode from the current root node to the child nodes
+        // that is in a direction closest to the up dir;
         std::shared_ptr<CDAGNode<T>> a_branch_node_ptr{};
-        T min_abs_dot_value{};
-        Vector3T a_root_pos(aBranchRootNodePtr->m_x, aBranchRootNodePtr->m_y, aBranchRootNodePtr->m_z);
+        T min_abs_dot_value = std::numeric_limits<T>::max();
+        // set the root node's position
+        Vector3t a_root_pos(aBranchRootNodePtr->m_x, aBranchRootNodePtr->m_y, aBranchRootNodePtr->m_z);
+
+        // iterate through all the child nodes of the current node to find the branch node
         for(const auto& n : aBranchRootNodePtr->m_child_nodes) {
-            if(n->m_branch_level > 0) // the node is already handled
+            if(n->m_branch_level > 0) // the node is already handled (default should be 0)
                 continue;
-            Vector3T a_dir = Vector3T(n->m_x, n->m_y, n->m_z) - a_root_pos;
+            Vector3t a_dir = Vector3t(n->m_x, n->m_y, n->m_z) - a_root_pos;
             a_dir.normalize();
-            T a_dot_value = abs(a_up_dir.dot(a_dir));
+            T a_dot_value = abs(aUpDir.dot(a_dir));
+            // record the node with the closet direction
             if(a_dot_value < min_abs_dot_value){
                 min_abs_dot_value = a_dot_value;
                 a_branch_node_ptr = n;
             }
         }
+
+        // add the found branch node
         a_branch_node_ptr->m_branch_level = aLevel;
         aBranch.add_node(a_branch_node_ptr);
 
         // Recursively extrat next branch node from this node
-        extract_branch_at_level(aLevel, a_branch_node_ptr, aBranch);
-    } else { // for other branches
+        extract_branch_at_level(aLevel, a_branch_node_ptr, aUpDir, aBranch);
+}
+
+template<typename T>
+void CDAGTree<T>::extract_branches_recursive(int aLevel,
+                                             std::vector<std::shared_ptr<CDAGNode<T>>>& aPreviousLevelBranch
+                                              ) {
+    CBranchLevelSet<T> a_branch_set;
+    // for each node of the current branch
+    for(auto& p : aPreviousLevelBranch) {
+        // for each child node of p, extract the branches rooted at p
+        for(auto& n : p->m_child_nodes){
+            // the node is alread assigned to an existing branch
+            if(n->m_branch_level > 0) continue;
+
+            // compute the up direction
+            Vector3t a_up_dir;
+            Vector3t parent_node_pos(p->m_x, p->m_y,p->m_z);
+            Vector3t current_node_pos(n->m_x, n->m_y, n->m_z);
+            a_up_dir = current_node_pos - parent_node_pos;
+            a_up_dir.normalize();
+
+            // extract a branch rooted at the node p
+            std::shared_ptr<CBranch<T>> a_branch_ptr(new CBranch<T>());
+            extract_branch_at_level(aLevel, p, a_up_dir, (*a_branch_ptr));
+            a_branch_set.add_branch(a_branch_ptr);
+
+            // recursively extract branches at the next level started from this node
+            extract_branches_recursive(aLevel+1, a_branch_ptr->get_branch_nodes());
+        }
 
     }
+
+    m_branches_array.push_back(a_branch_set);
 }
+
+template<typename T>
+void CDAGTree<T>::extract_branches() {
+    // extract the trunk branch
+    int a_level(1);
+    std::shared_ptr<CBranch<T>> trunk_branch_ptr(new CBranch<T>());
+    Vector3t a_up_dir(0, 1, 0);
+    extract_branch_at_level(a_level, m_node_array[0], a_up_dir, (*trunk_branch_ptr));
+    CBranchLevelSet<T> trunk_branch_set;
+    trunk_branch_set.add_branch(trunk_branch_ptr);
+    m_branches_array.push_back(trunk_branch_set);
+
+    // extract other branchs started from the trunk
+    extract_branches_recursive(a_level+1, trunk_branch_ptr->get_branch_nodes());
+}
+
 
 #endif // CDAGTREE_H
--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@ using namespace std;
 int main()
 {
     CDAGTree<float> a_tree;
-    cout << "Hello World!" << endl;
+    if(a_tree.load_tree_file("/home/yinhui/Projects/Qt/Tree3DViewer/TestData/Tree1.tree")) {
+        cout << "The tree file is read successfully!\n";
+        cout << "Total number of tree nodes: " << a_tree.get_total_num_of_nodes() << endl;
+        a_tree.extract_branches();
+        cout << "Total number of tree branch levels: " << a_tree.get_total_num_of_branch_levels() << endl;
+    } else {
+        cout << "Failed read the tree file\n";
+    }
+
     return 0;
 }
